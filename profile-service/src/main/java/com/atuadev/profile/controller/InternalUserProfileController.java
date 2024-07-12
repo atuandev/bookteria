b815e22d21bdf8dea62fc987b2a4f3e8c5933702
Boi <anhtuan03.dev@gmail.com>
2024-07-12 23:27:23 +0700

    feat: add api gateway

--- profile-service/src/main/java/com/atuadev/profile/controller/InternalUserProfileController.java
+++ profile-service/src/main/java/com/atuadev/profile/controller/InternalUserProfileController.java
@@ -1,21 +1,22 @@
 package com.atuadev.profile.controller;
 
+import com.atuadev.profile.dto.request.ProfileCreationRequest;
 import com.atuadev.profile.dto.response.UserProfileResponse;
 import com.atuadev.profile.service.UserProfileService;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
-public class UserProfileController {
+public class InternalUserProfileController {
     UserProfileService userProfileService;
 
-    @GetMapping("/users/{profileId}")
-    UserProfileResponse getUserProfile(@PathVariable String profileId) {
-        return userProfileService.getProfile(profileId);
+    @PostMapping("/internal/users")
+    UserProfileResponse createUserProfile(@RequestBody ProfileCreationRequest request) {
+        return userProfileService.createProfile(request);
     }
 }
